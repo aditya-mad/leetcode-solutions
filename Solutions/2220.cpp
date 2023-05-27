e58d8356c0def147d5b25eaf84be896be9e8ee1a
aditya-mad <maddulapalliaditya@gmail.com>
2023-05-27 12:31:59 +0530

    2220 27 may

---
+++ Solutions/2220.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int minBitFlips(int start, int goal)
+    {
+        int ans = 0;
+        for (int i = 0; i < 31; i++)
+        {
+            if ((start & (1 << i)) != (goal & (1 << i)))
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};

82becf464abe2e7f4ba003e653ff41213eb3d577
aditya-mad <maddulapalliaditya@gmail.com>
2023-05-29 23:14:32 +0530

    2433 29May

---
+++ Solutions/2433.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    vector<int> findArray(vector<int> &pref)
+    {
+        int last = pref[0];
+
+        for (int i = 1; i < pref.size(); i++)
+        {
+            pref[i] ^= last;
+            last ^= pref[i];
+        }
+
+        return pref;
+    }
+};

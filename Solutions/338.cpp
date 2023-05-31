762cec1044eca4231b72c8b6df409334b7476222
aditya-mad <maddulapalliaditya@gmail.com>
2023-05-31 19:53:38 +0530

    338 updates 31 May

--- Solutions/338.cpp
+++ Solutions/338.cpp
@@ -3,11 +3,13 @@ class Solution
 public:
     vector<int> countBits(int n)
     {
-        vector<int> ans;
-        for (int i = 0; i <= n; i++)
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 1; i < n + 1; i++)
         {
-            ans.push_back(__builtin_popcount(i));
+            ans[i] = ans[(i >> 1)] + (i % 2);
         }
+
         return ans;
     }
 };

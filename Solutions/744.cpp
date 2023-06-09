a52d6b9be5e1820e4dddae5f78da8d05ee5ce0ee
aditya-mad <maddulapalliaditya@gmail.com>
2023-06-09 14:19:49 +0530

    9 Jun 2023 - 14:19:46

---
+++ Solutions/744.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        char ans = '{';
+
+        for (int i = 0; i < letters.size(); i++)
+        {
+            if (letters[i] > target)
+            {
+                ans = min(ans, letters[i]);
+            }
+        }
+
+        return (ans == '{') ? (letters[0]) : (ans);
+    }
+};

b50303add7fe7a14f634f4288406cbf5bd9dbad1
aditya-mad <maddulapalliaditya@gmail.com>
2023-10-15 21:48:22 +0530

    15 Oct 2023 - 21:48:21

--- Solutions/2099. Find Subsequence of Length K With the Largest Sum.cpp
+++ Solutions/2099. Find Subsequence of Length K With the Largest Sum.cpp
@@ -3,24 +3,21 @@ class Solution
 public:
     vector<int> maxSubsequence(vector<int> &nums, int k)
     {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> ind;
-        priority_queue<pair<int, int>> temp;
+        vector<pair<int, int>> temp;
         vector<int> ans;
 
+        auto check = [&](const pair<int, int> &a, const pair<int, int> &b)
+        {
+            return a.second < b.second;
+        };
+
         for (int i = 0; i < nums.size(); i++)
-            temp.push({nums[i], i});
+            temp.push_back({nums[i], i});
+        sort(temp.rbegin(), temp.rend());
+        sort(temp.begin(), temp.begin() + k, check);
 
         for (int i = 0; i < k; i++)
-        {
-            ind.push({temp.top().second, temp.top().first});
-            temp.pop();
-        }
-
-        while (!ind.empty())
-        {
-            ans.push_back(ind.top().second);
-            ind.pop();
-        }
+            ans.push_back(temp[i].first);
 
         return ans;
     }

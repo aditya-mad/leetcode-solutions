1f865bd2cfb7c6be7de21e36daf54ddf5d3d8f63
aditya-mad <maddulapalliaditya@gmail.com>
2023-12-03 19:08:47 +0530

    3 Dec 2023 - 19:08:46

---
+++ Solutions/37. Sudoku Solver.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    bool validate(vector<vector<char>> &board, char c, int row, int col)
+    {
+        for (char i = 0; i < 9; i++)
+        {
+            if (board[i][col] == c)
+                return false;
+            if (board[row][i] == c)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    for (char k = '1'; k <= '9'; k++)
+                    {
+                        if (validate(board, k, i, j))
+                        {
+                            board[i][j] = k;
+                            if (solve(board))
+                                return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        solve(board);
+    }
+};

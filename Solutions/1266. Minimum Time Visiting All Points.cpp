d518cf3424d0af4347d9cf8a6192dc8444ecd81b
aditya-mad <maddulapalliaditya@gmail.com>
2023-12-03 10:20:21 +0530

    3 Dec 2023 - 10:20:20

---
+++ Solutions/1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>> &points)
+    {
+        int ans = 0, size = points.size();
+
+        for (int i = 1; i < size; i++)
+        {
+            ans += max(abs(points[i - 1][0] - points[i][0]), abs(points[i - 1][1] - points[i][1]));
+        }
+
+        return ans;
+    }
+};

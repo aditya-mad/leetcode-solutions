09f55109d134004b5c029abafad49ecc252ce458
aditya-mad <maddulapalliaditya@gmail.com>
2023-12-20 19:20:06 +0530

    20 Dec 2023 - 19:20:05

---
+++ Solutions/2966. Divide Array Into Arrays With Max Difference.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<vector<int>> divideArray(vector<int> &nums, int k)
+    {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        for (int i = 0; i < size; i += 3)
+        {
+            if (nums[i + 2] - nums[i] > k)
+                return {};
+            ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+        }
+
+        return ans;
+    }
+};

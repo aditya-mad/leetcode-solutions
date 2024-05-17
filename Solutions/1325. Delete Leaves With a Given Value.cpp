d8412ce9a36c31c4d487a78987ff2988455cbe37
aditya-mad <adityamaddulapalli@gmail.com>
2024-05-17 19:31:32 +0530

    17 May 2024 - 19:31:31

---
+++ Solutions/1325. Delete Leaves With a Given Value.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    TreeNode *removeLeafNodes(TreeNode *root, int target)
+    {
+        if (!root)
+            return NULL;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        return !root->left && !root->right && root->val == target ? NULL : root;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */

1c83cfc6b2af160b0f4bc7bdd2b44f5b4ddec737
aditya-mad <adityamaddulapalli@gmail.com>
2024-07-10 13:45:17 +0530

    10 Jul 2024 - 13:45:14

--- Solutions/1598. Crawler Log Folder.cpp
+++ Solutions/1598. Crawler Log Folder.cpp
@@ -3,17 +3,16 @@ class Solution
 public:
     int minOperations(vector<string> &logs)
     {
-        stack<string> memo;
+        int memo = 0;
         for (auto x : logs)
         {
             if (x[0] == '.' && x.size() == 2)
                 continue;
-            else if (x[0] == '.' && x.size() == 3 && !memo.empty())
-                memo.pop();
+            else if (x[0] == '.' && x.size() == 3 && memo > 0)
+                memo--;
             else if (x[0] != '.')
-                memo.push(x);
+                memo++;
         }
-
-        return memo.size();
+        return memo;
     }
 };
